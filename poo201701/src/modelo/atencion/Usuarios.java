e78c14fb9902b06e030a0b7562fa26c86d3fd66a
20102122476 <20102122476@LAB102-EQ13.UNISIMONBOLIVAR.LAB>
2017-05-12 19:21:26 -0500

    agregado modelo lectura

--- poo201701/src/modelo/atencion/Usuarios.java
+++ poo201701/src/modelo/atencion/Usuarios.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package modelo.lectura;
+package modelo.atencion;
 
 /**
  *
- * @author    
+ * @author 20102122476
  */
-public class prueba {
+public class Usuarios {
     
 }
